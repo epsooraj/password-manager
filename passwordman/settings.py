8c81ee86c3e33be845ab85e3cb8782405d8bac62
Sooraj Ep <epsooraj4@gmail.com>
2022-08-11 00:46:37 +0530

    APP

--- passwordman/settings.py
+++ passwordman/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'password.apps.PasswordConfig',
+    'organization.apps.OrganizationConfig'
 ]
 
 MIDDLEWARE = [
