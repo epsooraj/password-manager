e192d637efae9e12c60d1039dbc6d522056c6446
Sooraj Ep <epsooraj4@gmail.com>
2022-08-11 10:58:46 +0530

    Organization app, user app

--- passwordman/settings.py
+++ passwordman/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'password.apps.PasswordConfig',
-    'organization.apps.OrganizationConfig'
+    'organization.apps.OrganizationConfig',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
