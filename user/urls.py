17a40456f7b4917c3c0dbbff4ed9171d28b26c42
Sooraj Ep <epsooraj4@gmail.com>
2022-08-12 11:59:06 +0530

    User registration

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from rest_framework.authtoken import views
+from rest_framework.authtoken import views as rauth_views
+
+from . import views as user_views
 
 urlpatterns = [
-    path('login/', views.obtain_auth_token)
+    path('login/', rauth_views.obtain_auth_token, name='login'),
+    path('register/', user_views.RegisterView.as_view(), name='register'),
+
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from . import serializers
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = serializers.RegisterSerializer
