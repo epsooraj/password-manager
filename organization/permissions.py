7aacf7ff09f8b9d049bb176ed31740444322abc7
Sooraj Ep <epsooraj4@gmail.com>
2022-08-12 13:50:17 +0530

    Exception handling in permission

--- organization/permissions.py
+++ organization/permissions.py
@@ -1,3 +1,4 @@
+from re import L
 from rest_framework import permissions
 from . import models
 
@@ -12,24 +13,33 @@ class OwnerOrUserInOrganizationPermission(permissions.BasePermission):
         if obj.user == request.user:
             return True
 
-        # Check if user has access rights
-        # Read
-        if request.method in permissions.SAFE_METHODS:
-            return len(obj.members.filter(user__in=[request.user])) > 0
+        try:
+            # Check if user has access rights
+            # Read
+            if request.method in permissions.SAFE_METHODS:
+                return len(obj.members.filter(user__in=[request.user])) > 0
 
-        # Write
-        return len(obj.members.filter(user__in=[request.user], permission='w')) > 0
+            # Write
+            return len(obj.members.filter(user__in=[request.user], permission='w')) > 0
+        except:
+            return False
 
 
 class OwnerOnlySharePermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated:
-            oid = request.resolver_match.kwargs.get('oid')
-            return len(models.Organization.objects.filter(id=oid, user=request.user)) > 0
+            try:
+                oid = request.resolver_match.kwargs.get('oid')
+                return len(models.Organization.objects.filter(id=oid, user=request.user)) > 0
+            except:
+                pass
 
     def has_object_permission(self, request, view, obj):
-        if obj.organization_set.all().first().user == request.user:
-            return True
+        try:
+            if obj.organization_set.all().first().user == request.user:
+                return True
+        except:
+            pass
 
         return False
 
@@ -41,21 +51,27 @@ class OwnerOrUserInOrgPasswordPermission(permissions.BasePermission):
             if len(models.Organization.objects.filter(id=oid, user=request.user)) > 0:
                 return True
 
-            if request.method == 'GET':
-                return len(models.Organization.objects.filter(id=oid).first(
-                ).members.filter(user__in=[request.user])) > 0
-            else:
-                return len(models.Organization.objects.filter(id=oid).first().members.filter(
-                    user__in=[request.user], permission='w')) > 0
+            try:
+                if request.method == 'GET':
+                    return len(models.Organization.objects.filter(id=oid).first(
+                    ).members.filter(user__in=[request.user])) > 0
+                else:
+                    return len(models.Organization.objects.filter(id=oid).first().members.filter(
+                        user__in=[request.user], permission='w')) > 0
+            except:
+                return False
 
     def has_object_permission(self, request, view, obj):
         if obj.organization_set.all().first().user == request.user:
             return True
 
-        # Check if user has access rights
-        # Read
-        if request.method in permissions.SAFE_METHODS:
-            return len(obj.organization_set.all().first().members.filter(user__in=[request.user])) > 0
+        try:
+            # Check if user has access rights
+            # Read
+            if request.method in permissions.SAFE_METHODS:
+                return len(obj.organization_set.all().first().members.filter(user__in=[request.user])) > 0
 
-        # Write
-        return len(obj.organization_set.all().first().members.filter(user__in=[request.user], permission='w')) > 0
+            # Write
+            return len(obj.organization_set.all().first().members.filter(user__in=[request.user], permission='w')) > 0
+        except:
+            return False
--- password/permissions.py
+++ password/permissions.py
@@ -12,13 +12,16 @@ class OwnerOrUserWithOwnersPermission(permissions.BasePermission):
         if obj.user == request.user:
             return True
 
-        # Check if user has access rights
-        # Read
-        if request.method in permissions.SAFE_METHODS:
-            return len(obj.access_users.filter(user__in=[request.user])) > 0
+        try:
+            # Check if user has access rights
+            # Read
+            if request.method in permissions.SAFE_METHODS:
+                return len(obj.access_users.filter(user__in=[request.user])) > 0
 
-        # Write
-        return len(obj.access_users.filter(user__in=[request.user], permission='w')) > 0
+            # Write
+            return len(obj.access_users.filter(user__in=[request.user], permission='w')) > 0
+        except:
+            return False
 
 
 class ShareOwnerOnlyPermission(permissions.BasePermission):
@@ -29,12 +32,11 @@ class ShareOwnerOnlyPermission(permissions.BasePermission):
             return len(models.Password.objects.filter(id=pid, user=request.user)) > 0
 
     def has_object_permission(self, request, view, obj):
-        # if request.user.is_authenticated:
-        #     # If corresponding password object belong to the user
-        #     pid = request.resolver_match.kwargs.get('pid')
-        #     return len(models.Password.objects.filter(id=pid, user=request.user)) > 0
 
-        if obj.password_set.all().first().user == request.user:
-            return True
+        try:
+            if obj.password_set.all().first().user == request.user:
+                return True
+        except:
+            pass
 
         return False
