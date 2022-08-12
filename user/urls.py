1c33d8c177ea61b0a4e205d8c5d8f01730c2ff23
Sooraj Ep <epsooraj4@gmail.com>
2022-08-12 13:33:46 +0530

    Static files to url

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework.authtoken import views as rauth_views
 
 from . import views as user_views
@@ -6,5 +8,4 @@ from . import views as user_views
 urlpatterns = [
     path('login/', rauth_views.obtain_auth_token, name='login'),
     path('register/', user_views.RegisterView.as_view(), name='register'),
-
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
