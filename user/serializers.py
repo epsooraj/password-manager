6ce9553b594f474d1ae9928b69323ea183a4ae51
Sooraj Ep <epsooraj4@gmail.com>
2022-08-12 18:02:57 +0530

    Using createuser

--- user/serializers.py
+++ user/serializers.py
@@ -37,14 +37,15 @@ class RegisterSerializer(serializers.ModelSerializer):
         return attrs
 
     def create(self, validated_data):
-        user = User.objects.create(
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             first_name=validated_data['first_name'],
-            last_name=validated_data['last_name']
+            last_name=validated_data['last_name'],
+            password=validated_data['password'],
         )
 
-        user.set_password(validated_data['password'])
+        # user.set_password(validated_data['password'])
         user.save()
 
         return user
